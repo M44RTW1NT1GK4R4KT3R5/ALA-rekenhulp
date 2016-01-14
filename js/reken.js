cfc134c503015a78477c7599e1258e605527cbe1
patrick ramge <patrick ramge>
2016-01-14 11:37:25 +0100

    helemaal afgerond

--- js/reken.js
+++ js/reken.js
@@ -1,5 +1,8 @@
 // hard aan t werk
+var antwoorden = [];
 function toetsmaker(niveau){ /*zet bij niveau het gekozen niveau uit de dropdownlist*/
+    var tafels = [1,2,3,4,5,6,7,8,9,10,11,12];
+    antwoorden = [];
     document.getElementById("resultaat").innerHTML =null;
     if (niveau <1){
         document.getElementById("nakijk").disabled = true;
@@ -12,10 +15,13 @@ function toetsmaker(niveau){ /*zet bij niveau het gekozen niveau uit de dropdown
     }else{
         document.getElementById("nakijk").disabled = false;
         for (i=1;i<=12;i++){ //deze loop maakt alle vragen
-            document.getElementById('s'+i).innerHTML = i+'x'+niveau+'=';
+            var getal = Math.floor((Math.random() * tafels.length));
+            document.getElementById('s'+i).innerHTML = tafels[getal]+'x'+niveau+'=';
             document.getElementById('a' + i).value = null;
             document.getElementById('a' + i).disabled = false;
             document.getElementById('a' + i).style.background = "#EEE";
+            antwoorden.splice(i-1,0,tafels[getal]*niveau);
+            tafels.splice(getal,1);
         }
     }
 }
@@ -23,7 +29,7 @@ function nakijken(niveau){
     var goed = 0;
     var fout = 0;
     for (i=1;i<=12;i++) { //deze loop controleert alle vragen
-        if (document.getElementById('a'+i).value == niveau*i){ //als correct
+        if (document.getElementById('a'+i).value == antwoorden[i-1]){ //als correct
             goed++;
             document.getElementById('a'+i).style.background = "#0f0";
         } else { //als fout
